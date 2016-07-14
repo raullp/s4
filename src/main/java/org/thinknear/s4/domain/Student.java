1cb31adac64eee6305936ec634c186c90335de4b
Raul Lopez <raul.lp@gmail.com>
2016-07-14 08:03:13 -0400

    Fixing imports in Student entity.

--- src/main/java/org/thinknear/s4/domain/Student.java
+++ src/main/java/org/thinknear/s4/domain/Student.java
@@ -3,14 +3,11 @@ package org.thinknear.s4.domain;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
-import java.time.ZonedDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 /**
