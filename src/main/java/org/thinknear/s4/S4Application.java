ebca370d17e878e82f4e76d9567d53392d04ca62
Raul Lopez <raul.lp@gmail.com>
2016-07-13 22:51:54 -0400

    Enable Jpa Auditing

--- src/main/java/org/thinknear/s4/S4Application.java
+++ src/main/java/org/thinknear/s4/S4Application.java
@@ -2,8 +2,10 @@ package org.thinknear.s4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class S4Application {
 
 	public static void main(String[] args) {
