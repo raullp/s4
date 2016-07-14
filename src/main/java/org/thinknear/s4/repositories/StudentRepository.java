defc9d5ebf77ab329b4067cd93c6e8284e7da0ec
Raul Lopez <raul.lp@gmail.com>
2016-07-14 18:13:36 -0400

    Disable DELETE Students api

--- src/main/java/org/thinknear/s4/repositories/StudentRepository.java
+++ src/main/java/org/thinknear/s4/repositories/StudentRepository.java
@@ -1,10 +1,21 @@
 package org.thinknear.s4.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.thinknear.s4.domain.Student;
 
 /**
  * Created by raul on 7/13/16.
  */
+@RepositoryRestResource
 public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Long id);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Student student);
 }
