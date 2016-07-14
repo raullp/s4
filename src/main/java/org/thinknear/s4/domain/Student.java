ecb7a9560211861bb89ebc1d795bc1286bd5e8a6
Raul Lopez <raul.lp@gmail.com>
2016-07-13 22:53:28 -0400

    Add auditing for Student class, lastModifiedBy and modifiedBy will be update once User/Security configuration is done.

--- src/main/java/org/thinknear/s4/domain/Student.java
+++ src/main/java/org/thinknear/s4/domain/Student.java
@@ -3,15 +3,19 @@ package org.thinknear.s4.domain;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Version;
+import javax.persistence.*;
+import java.time.ZonedDateTime;
 
 /**
  * Created by raul on 7/13/16.
  */
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Data
 public class Student {
@@ -30,4 +34,16 @@ public class Student {
     @NotBlank
     @Length(min = 1, max = 64)
     private String lastName;
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private ZonedDateTime createdDate;
+
+    @LastModifiedBy
+    private String modifiedBy;
+
+    @LastModifiedDate
+    private ZonedDateTime modifiedDate;
 }
